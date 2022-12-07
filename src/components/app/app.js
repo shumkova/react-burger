2ac38d7b934a9b0eb9fd439a3dc53508232df005
Aleksandra Shumkova <shumkaleksandra@yandex.ru>
2022-12-07 16:25:33 +0700

    feat: подключает к api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,36 +5,66 @@ import data from '../../utils/data.json';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import {Button, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
+const URL = 'https://norma.nomoreparties.space/api/ingredients';
+
 const App = () => {
   const [state, setState] = React.useState({
-    chosen: {
-      bun: data.find((item) => item.type === 'bun'),
-      inner: data.filter((item) => item.type !== 'bun').slice(0, 1),
-    },
+    ingredients: [],
+    chosen: {},
   })
+
+  React.useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setState({
+          ...state,
+          ingredients: res.data,
+          chosen: {
+            bun: res.data.find((item) => item.type === 'bun'),
+            inner: res.data.filter((item) => item.type !== 'bun').slice(0, 3),
+          },
+        })
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
+  }, [])
 
   return (
     <div className="page">
       <AppHeader activeLink={'main'}/>
       <main className="main container pt-10">
-        <h1 className="main__title text text_type_main-large mb-5">Соберите бургер</h1>
-        <div className="two-columns">
-          <BurgerIngredients ingredients={data}/>
 
-          <div className="order">
-            <BurgerConstructor bun={state.chosen.bun} inner={state.chosen.inner}/>
-            <div className="order__footer mt-5 pr-4">
-              <p className="order__total mr-10">
-                <span className="order__price text text_type_digits-medium">610</span>
-                <CurrencyIcon type={"primary"} />
-              </p>
-              <Button htmlType="button" type="primary" size="large">
-                Оформить заказ
-              </Button>
-            </div>
-          </div>
+        {state.ingredients.length
+          ? (
+            <>
+              <h1 className="main__title text text_type_main-large mb-5">Соберите бургер</h1>
+              <div className="two-columns">
+                <BurgerIngredients ingredients={state.ingredients}/>
 
-        </div>
+                <div className="order">
+                  <BurgerConstructor bun={state.chosen.bun} inner={state.chosen.inner}/>
+                  <div className="order__footer mt-5 pr-4">
+                    <p className="order__total mr-10">
+                      <span className="order__price text text_type_digits-medium">610</span>
+                      <CurrencyIcon type={"primary"} />
+                    </p>
+                    <Button htmlType="button" type="primary" size="large">
+                      Оформить заказ
+                    </Button>
+                  </div>
+                </div>
+
+              </div>
+            </>
+          )
+          : (
+            <>Извниите, произошла ошибка</>
+          )
+        }
+
       </main>
     </div>
   )
